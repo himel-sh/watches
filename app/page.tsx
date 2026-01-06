2e16a586bb430454228363a3b031b0ef244c5b5a
Himel <shahriarhasan221@gmail.com>
2026-01-07 05:54:28 +0600

    input to git

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/Hero";
+import ProductList from "@/components/ProductList";
+
+export default function Home() {
+  return (
+    <div className="bg-[#F8F9FA]">
+      <Hero />
+
+      <h2 className="w-full text-center text-2xl md:text-4xl font-semibold py-6">
+        All Products
+      </h2>
+      <ProductList />
+    </div>
+  );
+}
